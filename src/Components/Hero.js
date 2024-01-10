a30383363f4eac297e0cd085a663755897c323f2
Rizkiputraramadhan <rizkikuliit@gmail.com>
2024-01-11 03:26:10 +0700

    updated icon alert

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Hero = () => {
+  const initialIsActive = localStorage.getItem("isActive") === "true";
+  const [isActive, setIsActive] = useState(initialIsActive);
+  const [showAlert, setShowAlert] = useState(false);
+  const handleButtonClick = () => {
+    if (!isActive) {
+      setIsActive(true);
+      localStorage.setItem("isActive", "true");
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 3000); 
+    }
+  };
+
+  // Membersihkan localStorage saat komponen di-unmount
+  useEffect(() => {
+    return () => localStorage.removeItem("isActive");
+  }, []);
   return (
     <>
       <link
@@ -29,41 +47,32 @@ const Hero = () => {
             <p className="text-sm text-slate-300">Fullstack web Development</p>
             <div className="p-2">
               <a className="hover:text-yellow-700" href="">
-                <i class="fa-brands fa-whatsapp m-1"></i>
+                <i class="fa-brands fa-whatsapp m-1 text-xl"></i>
               </a>
               <a className="hover:text-yellow-700" href="">
-                <i class="fa-brands fa-whatsapp m-1"></i>
+                <i class="fa-brands fa-instagram m-1 text-xl"></i>
               </a>
               <a className="hover:text-yellow-700" href="">
-                <i class="fa-brands fa-whatsapp m-1"></i>
+                <i class="fa-brands fa-linkedin m-1 text-xl"></i>
               </a>
               <a className="hover:text-yellow-700" href="">
-                <i class="fa-brands fa-whatsapp m-1"></i>
+                <i class="fa-brands fa-github m-1 text-xl"></i>
               </a>
             </div>
           </div>
           <div className="text-start p-5">
-            <button className="btn">
+            <button className="btn" onClick={handleButtonClick}>
               Dukungan
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
+              <i
+                class={`fa-regular fa-thumbs-up text-xl ${
+                  isActive ? "text-success" : ""
+                }`}
+              ></i>
             </button>
             <div className="my-3">
               <div className="flex py-2">
                 <span className="flex items-center rounded-l-lg justify-center border border-gray-400 border-r-0 py-2 px-5 bg-success">
-                  <i class="fa-brands fa-searchengin fa-bounce text-xl"></i>
+                  <i class="fa-brands fa-searchengin text-xl"></i>
                 </span>
                 <input
                   className="input-warning w-full max-w-xs border rounded-r-lg border-gray-400 p-3 focus:outline-none"
@@ -73,6 +82,13 @@ const Hero = () => {
               </div>
             </div>
           </div>
+          {showAlert && (
+            <div className="absolute top-0 right-0 mt-4 mr-4">
+              <div className="text-success p-4 rounded">
+              <i class="fa-solid fa-thumbs-up fa-bounce text-8xl"></i>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
