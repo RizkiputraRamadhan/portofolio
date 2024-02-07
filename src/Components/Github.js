62e889ff0e0300f1cf4279033c77fc32034f06b3
Rizkiputraramadhan <rizkikuliit@gmail.com>
2024-02-07 19:33:07 +0700

    update v2

--- src/Components/Github.js
+++ src/Components/Github.js
@@ -2,52 +2,91 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 const GitHub = () => {
   const [projects, setProjects] = useState([]);
+  console.log(projects);
 
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/users/RizkiputraRamadhan/repos`,
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+            },
+          }
+        );
 
-useEffect(() => {
-  const fetchProjects = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/users/RizkiputraRamadhan/repos`,
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
-          },
-        }
-      );
+        const projectsWithLanguages = await Promise.all(
+          response.data.map(async (project) => {
+            const languagesResponse = await axios.get(
+              `${process.env.REACT_APP_API_URL}/repos/RizkiputraRamadhan/${project.name}/languages`,
+              {
+                headers: {
+                  Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+                },
+              }
+            );
 
-      const projectsWithLanguages = await Promise.all(
-        response.data.map(async (project) => {
-          const languagesResponse = await axios.get(
-            `${process.env.REACT_APP_API_URL}/repos/RizkiputraRamadhan/${project.name}/languages`,
-            {
-              headers: {
-                Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
-              },
-            }
-          );
+            return {
+              ...project,
+              languages: Object.keys(languagesResponse.data),
+            };
+          })
+        );
 
-          return {
-            ...project,
-            languages: Object.keys(languagesResponse.data),
-          };
-        })
-      );
+        setProjects(projectsWithLanguages);
+      } catch (error) {
+        console.error("Error fetching GitHub projects:", error);
+      }
+    };
 
-      setProjects(projectsWithLanguages);
-    } catch (error) {
-      console.error("Error fetching GitHub projects:", error);
-    }
-  };
-
-  fetchProjects();
-}, []);
+    fetchProjects();
+  }, []);
 
   return (
     <div className="justify-center">
       <div className="text-2xl w-34 mb-3 font-bold sm:text-4xl text-gray-50 divider divider-primary ">
         Repository Github
       </div>
+      <div className=" flex flex-col max-w-lg p-6 m-5 divide-y rounded-md divide-gray-300 dark:bg-gray-800 text-gray-100 border">
+        <div className="flex justify-between p-4 w-">
+          <div className="flex space-x-4">
+            <div>
+              <img
+                src="https://i.ibb.co/cJ8G4XP/profile.jpg"
+                alt=""
+                className="object-cover w-12 h-12 rounded-full bg-gray-500"
+              />
+            </div>
+            <div>
+              <h4 className="font-bold">RizkiputraRamadhan</h4>
+              <span className="text-xs text-gray-200">
+                Rizki Putra R ( Team Kuli It ) <br />
+              </span>
+              <h2 className="text-lg font-bold text-yellow-400">
+                {projects.length}
+                <span className="text-xs"> Repo Publish</span>
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className="text-gray-600"></div>
+      </div>
+      {projects.length === 0 ? (
+        <>
+          <div className="my-5">
+            <div className="text-lg font-bold text-yellow-400 items-center animate-pulse text-center ">
+              Sedang mengambil data dari Github{" "}
+              <i class="fa-solid fa-spinner fa-spin-pulse"></i>
+            </div>
+            <div className="flex my-5 items-center justify-center space-x-2">
+              <div className="w-4 h-4 rounded-full animate-pulse bg-violet-600"></div>
+              <div className="w-4 h-4 rounded-full animate-pulse bg-violet-600"></div>
+              <div className="w-4 h-4 rounded-full animate-pulse bg-violet-600"></div>
+            </div>
+          </div>
+        </>
+      ) : (
       <ul>
         <section className="py-6 dark:bg-gray-800 ">
           <div className="container flex flex-col justify-center p-4 mx-auto">
@@ -84,7 +123,7 @@ useEffect(() => {
                       </div>
                       <div className=" grid grid-cols-3 gap-3 lg:grid-cols-3 sm:grid-cols-2">
                         {project.languages.map((language, index) => (
-                            <>
+                          <>
                             <button className="btn  btn-outline btn-success btn-xs">
                               {language}
                               {index !== project.languages.length - 1 && " "}
@@ -100,6 +139,7 @@ useEffect(() => {
           </div>
         </section>
       </ul>
+      )}
     </div>
   );
 };
